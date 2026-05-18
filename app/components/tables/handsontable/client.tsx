42d8622243ae4acd08eccd8994ed1a1f590386eb
lilpacy <revivedtomorrow@gmail.com>
2026-05-18 22:44:05 +0900

    feat: enable handsontable row selection

--- app/components/tables/handsontable/client.tsx
+++ app/components/tables/handsontable/client.tsx
@@ -421,7 +421,8 @@ export function HandsontableSalesTableClient({
         data={rowsRef.current}
         columns={columns}
         colHeaders={colHeaders}
-        rowHeaders={false}
+        rowHeaders
+        rowHeaderWidth={44}
         width="100%"
         height={360}
         stretchH="none"
